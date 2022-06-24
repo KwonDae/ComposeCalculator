7d322bda1775088322d0779a7a39ac886cbb1355
KwonDae <green201402317@gmail.com>
2022-06-24 19:55:53 +0900

    Calculator 버튼 구현, viewModel 설정

--- app/src/main/java/com/example/composecalculator/CalculatorViewModel.kt
+++ app/src/main/java/com/example/composecalculator/CalculatorViewModel.kt
@@ -10,4 +10,35 @@ class CalculatorViewModel: ViewModel() {
     var state by mutableStateOf(CalculatorState())
         private set
 
+    fun onAction(action: CalculatorAction) {
+        when(action) {
+            is CalculatorAction.Number -> enterNumber(action.number)
+            is CalculatorAction.Clear -> state = CalculatorState()
+            is CalculatorAction.Delete -> perfromCalculation()
+            is CalculatorAction.Decimal -> enterDecimal()
+            is CalculatorAction.Calculate -> perfromCalculation()
+            is CalculatorAction.Operation -> enterOperation(action.operation)
+        }
+    }
+
+
+    private fun enterNumber(number: Int) {
+
+    }
+
+    private fun perfromCalculation() {
+        TODO("Not yet implemented")
+    }
+
+    private fun enterDecimal() {
+        TODO("Not yet implemented")
+    }
+
+    private fun enterOperation(operation: CalculatorOperation) {
+
+    }
+
+
+
+
 }
--- app/src/main/java/com/example/composecalculator/MainActivity.kt
+++ app/src/main/java/com/example/composecalculator/MainActivity.kt
@@ -3,20 +3,40 @@ package com.example.composecalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.composecalculator.ui.theme.ComposeCalculatorTheme
+import com.example.composecalculator.ui.theme.MediumGray
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeCalculatorTheme {
+                val viewModel = viewModel<CalculatorViewModel>()
+                val state = viewModel.state
+                val buttonSpacing = 8.dp
+                Calculator(
+                    state = state,
+                    onAction = viewModel::onAction,
+                    buttonSpacing = buttonSpacing,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(MediumGray)
+                        .padding(16.dp)
+                )
 
             }
         }
